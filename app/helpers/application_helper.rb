a0dd781c71ccb9d10c13b5ac07d5b19d3042a5ab
leva37a <leva37a@yandex.ru>
2023-08-15 12:49:29 +0300

    Added Photo resource

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,34 @@ module ApplicationHelper
     end
   end
 
+  def event_photo(event)
+  photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.png')
+    end
+  end
+
   def type_icon(icon_class)
     content_tag 'span', '', class: "bi bi-#{icon_class}"
   end
